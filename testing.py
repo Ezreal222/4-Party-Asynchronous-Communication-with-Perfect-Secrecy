ebe72a17581d76534da48fbeeacb6d70d01ae838
Ezreal222 <yz7906>
2026-02-14 23:48:34 -0500

    Testing requirements

--- testing.py
+++ testing.py
@@ -1,1 +1,6 @@
-
+"""
+Testing program for 4-Party Asynchronous Communication Protocol
+- (Scenario (S.x)): only x, randomly chosen, parties repeatedly send L-length messages
+- x = {1,2,4}
+- Report average wasted pads 
+"""
